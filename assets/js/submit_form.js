f5735adf5ebe66af3108ffbf285fb61ff73ef334
Matt Kellock <matt.kellock@iress.com>
2019-09-27 06:36:45 +1000

    Changing form submit API endpoint

--- assets/js/submit_form.js
+++ assets/js/submit_form.js
@@ -1,38 +1,38 @@
 function submit_form() {
-	var message = {
-		name: $('#name').val(),
-		email: $('#email').val(),
-		message: $('#message').val()
-	};
+    var message = {
+        name: $('#name').val(),
+        email: $('#email').val(),
+        message: $('#message').val()
+    };
 
-	$('#name').prop('disabled', true);
-	$('#email').prop('disabled', true);
-	$('#message').prop('disabled', true);
-	$('submit').prop('disabled', true);
-	$('reset').prop('disabled', true);
+    $('#name').prop('disabled', true);
+    $('#email').prop('disabled', true);
+    $('#message').prop('disabled', true);
+    $('submit').prop('disabled', true);
+    $('reset').prop('disabled', true);
 
-	$('#contactform').validate();
+    $('#contactform').validate();
 
-	$('#message_result').html('Sending...');
+    $('#message_result').html('Sending...');
 
-	$.ajax({
-		url: 'https://api.mattkellock.com/contactform',
-		type: 'post',
-		contentType: 'application/json',
-		crossDomain: true,
-		dataType: 'json',
-		success: function(data) {
-			$('#message_result').html('Your message has been sent!');
-		},
-		error: function(data) {
-			$('#message_result').html('There was an error sending your message, please try again');
-		},
-		data: JSON.stringify(message)
-	});
+    $.ajax({
+        url: 'https://api.mattkellock.com/cosc2196-a2',
+        type: 'post',
+        contentType: 'application/json',
+        crossDomain: true,
+        dataType: 'json',
+        success: function(data) {
+            $('#message_result').html('Your message has been sent!');
+        },
+        error: function(data) {
+            $('#message_result').html('There was an error sending your message, please try again');
+        },
+        data: JSON.stringify(message)
+    });
 
-	$('#name').prop('disabled', false);
-	$('#email').prop('disabled', false);
-	$('#message').prop('disabled', false);
-	$('submit').prop('disabled', false);
-	$('reset').prop('disabled', false);
+    $('#name').prop('disabled', false);
+    $('#email').prop('disabled', false);
+    $('#message').prop('disabled', false);
+    $('submit').prop('disabled', false);
+    $('reset').prop('disabled', false);
 }
